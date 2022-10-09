2fe9012030b0b81fba457bd784931f4db0e465ad
Caio Rangel <caiorg2004@gmail.com>
2022-10-09 11:49:31 -0300

    tela de ME específica finalizada

--- Lenvanta/CounterStress/telasUsuario/MeEspecifica.js
+++ Lenvanta/CounterStress/telasUsuario/MeEspecifica.js
@@ -6,6 +6,7 @@ import {
   Image,
   TextInput,
   Pressable,
+  ScrollView
 } from 'react-native';
 
 import TopBar from '../components/TopBar';
@@ -17,6 +18,8 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import {vh,vw} from 'react-native-expo-viewport-units';
 
 import { LinearGradient } from 'expo-linear-gradient';
+
+const texto = "A meditação vipassana é uma das mais antigas práticas de centralidade da Índia. Ela se mostra como uma rota para se autotransformar usando a auto-observação para atingir esse caminho. Seu foco primário é a conexão entre o corpo e a mente do praticante. Isso pode ser experimentado através da atenção voltada às sensações físicas que movimentam o corpo e conduzem a mente. O caminho para a visualização acontece desde a raiz que unifica o ..." 
 
 export default function MeEspecifica({navigation}) {
 
@@ -31,25 +34,33 @@ export default function MeEspecifica({navigation}) {
         </Pressable>
 
       <View style = {styles.container}>
-
+      <ScrollView showsVerticalScrollIndicator={false}>
         <LinearGradient
         style={styles.parteSuperior}
         start={{x: 0,y: 0}}
         end={{x: 0, y: 1}}
-        colors={["#8E4FCD", "#5694fa"]}>
+        colors={["#8E4FCD", "#C4BFE7"]}>
 
 
            <MaterialCommunityIcons name = "meditation" size = {140} style = {styles.icone} /> 
-           <View style = {styles.circulo}></View>
+           <Text style = {styles.Titulo}>Vipassana</Text>
 
         
         </LinearGradient>
 
 
         <View style = {styles.parteInferior}>
-
+          
+            <Text style = {styles.texto}>{texto}</Text>
+            <View style = {styles.Fontes}>
+              <Text style = {styles.Fonte}>Fontes</Text>
+              <Text style = {styles.Link}>linkparateste.com</Text>
+              <Text style = {styles.Link}>linkparateste2.com</Text>
+              <Text style = {styles.Link}>linkparateste3.com</Text>
+            </View>
         </View>
-
+        
+        </ScrollView>
       </View>
     </>
     )
@@ -71,9 +82,8 @@ const styles = StyleSheet.create({
     },
 
     container: {
-      backgroundColor: 'red',
-      height: vh(100),
-      width: vw(100)
+      
+      
 
     },
 
@@ -82,17 +92,48 @@ const styles = StyleSheet.create({
       height: vh(30),
       width: vw(100),
       alignItems: 'center',
-      zIndex: 1
     },
 
     icone: {
-        marginTop: vh(1)
+      marginTop: vh(1)
     },
 
-    
+    Titulo: {
+
+      fontSize: vw(8),
+
+    },
+
 
     parteInferior: {
-        zIndex: 5
+      width: vw(100),
+      backgroundColor: '#C4BFE7'
+    },
+
+    texto: {
+      fontSize: vw(7),
+      marginHorizontal: vw(5.5),
+      marginTop: vh(4),
+      textAlign: 'justify',
+      
+    },
+
+    Fontes: {
+  
+      marginTop: vh(4),
+      marginBottom: vh(15),
+      marginLeft: vw(5.5),
+    },
+
+    Fonte: {
+      fontSize: vw(7),
+      
+      color: '#8E4FCD'
+    },
+
+    Link: {
+      fontSize: vw(7),
+      marginTop: vh(2)
     }
 
  
